1ee8eb2eb6072de2a7f47fa76289b9a0132cf7fe
Fabio Alcocer <fabioalcocersejas17@gmail.com>
2022-04-08 14:41:13 -0400

    ejercicio 1/5

---
+++ 5-functions.py
@@ -0,0 +1,12 @@
+import math
+
+def area_triangulo(base, altura):
+  return (base*altura)/2
+
+print("Area triangulo: ", area_triangulo(8,5))
+
+
+def area_circulo(radio):
+    return math.pi * radio**2
+
+print("Area circulo: ",area_circulo(7))
